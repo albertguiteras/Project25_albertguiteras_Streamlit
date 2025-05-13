5b90a07dfd9b9be650e2804e92a29006ca03e996
albertguiteras <albertguiteras91@gmail.com>
2025-05-13 15:08:08 +0200

    ENTREGA_1

--- src/app.py
+++ src/app.py
@@ -1,4 +1,31 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+from sklearn.datasets import load_iris
+import pandas as pd
+import numpy as np
+import joblib
+import os
 
-# your code here
+MODEL_PATH = "../models/knn_model_iris.pkl"
+
+model = joblib.load(MODEL_PATH)
+
+# Interfaz de usuario con Streamlit
+st.title("Predicción de Especies de Iris 🌸")
+
+st.write("Introduce las características de la flor:")
+
+# Inputs del usuario
+sepal_length = st.slider("Largo del sépalo (cm)", 4.0, 8.0, 5.1)
+sepal_width = st.slider("Ancho del sépalo (cm)", 2.0, 4.5, 3.5)
+petal_length = st.slider("Largo del pétalo (cm)", 1.0, 7.0, 1.4)
+petal_width = st.slider("Ancho del pétalo (cm)", 0.1, 2.5, 0.2)
+
+input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+
+# Predicción
+prediction = model.predict(input_data)[0]
+species = load_iris().target_names[prediction]
+
+# Resultado
+st.subheader("Predicción:")
+st.write(f"La flor es de la especie: **{species}**") 
